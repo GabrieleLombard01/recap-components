96356286a34baf53cfd40edd5bd3a62e8622b6b1
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2024-03-03 15:33:19 +0100

    ProductCard fix

--- src/app/ProductCard/ProductCard.component.ts
+++ src/app/ProductCard/ProductCard.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-ProductCard',
   templateUrl: './ProductCard.component.html',
-  styleUrls: ['./ProductCard.component.css']
+  styleUrls: ['./ProductCard.component.scss'],
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule]
 })
 export class ProductCardComponent implements OnInit {
 
--- src/app/Products/Products.component.ts
+++ src/app/Products/Products.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductCardComponent } from '../ProductCard/ProductCard.component';
 
 @Component({
   selector: 'app-Products',
   templateUrl: './Products.component.html',
-  styleUrls: ['./Products.component.css'],
-  standalone: true
+  styleUrls: ['./Products.component.scss'],
+  standalone: true,
+  imports: [ProductCardComponent]
 })
 export class ProductsComponent implements OnInit {
 
