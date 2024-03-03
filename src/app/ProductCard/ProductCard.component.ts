2eb178c5782dcf6634ffbb842d58d20360822cdb
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2024-03-03 16:45:07 +0100

    products add

--- src/app/ProductCard/ProductCard.component.ts
+++ src/app/ProductCard/ProductCard.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './product.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-ProductCard',
   templateUrl: './ProductCard.component.html',
   styleUrls: ['./ProductCard.component.scss'],
   standalone: true,
-  imports: [MatCardModule, MatButtonModule]
+  imports: [MatCardModule, CommonModule, HttpClientModule, MatButtonModule],
+  providers: [ProductService]
 })
 export class ProductCardComponent implements OnInit {
 
-  constructor() { }
+  products: any[] = [];
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data.prodottiPerCani;
+    });
   }
 
 }
